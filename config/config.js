423c5b9e1cc00365b141e12ed6c5b331948faf05
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-19 02:34:25 +0300

    Mongo Atlas Setup Completed

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
 export const Secret = "MySecret";
 
-export const MongoURI = "mongodb://127.0.0.1/media1"; // copied the connection string from mongosh. Creating a dababase called media
+// export const MongoURI = "mongodb://127.0.0.1/media1"; // copied the connection string from mongosh. Creating a dababase called media
+
+export const MongoURI = "mongodb+srv://Salma:salma123@comp229.wjzgekh.mongodb.net/media2?retryWrites=true&w=majority";
