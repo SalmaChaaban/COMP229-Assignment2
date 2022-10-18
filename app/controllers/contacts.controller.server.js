2a7b3635b9af3a5e1dc8f98140c6a4e8ad501821
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-10-18 22:47:26 +0300

    Display Contacts Add Page Completed

--- app/controllers/contacts.controller.server.js
+++ app/controllers/contacts.controller.server.js
@@ -1,6 +1,6 @@
 import contactModel from '../models/contacts.js';
 
-export function DisplayContactList(req, res, next) {
+export function DisplayContactsList(req, res, next) {
     contactModel.find(function(err, contactsCollection) {
         if(err) { // error checking
             console.error(err);
@@ -9,4 +9,8 @@ export function DisplayContactList(req, res, next) {
 
         res.render('index', {title: 'Contact List', page: '/contacts/list', contacts: contactsCollection});
     })
+}
+
+export function DisplayContactsAddPage(req, res, next) {
+    res.render('index', {title: 'Add Contact', page: 'contacts/edit', contact: {}});
 }
--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { DisplayContactList } from "../controllers/contacts.controller.server.js";
+import { DisplayContactsList, 
+    DisplayContactsAddPage } from "../controllers/contacts.controller.server.js";
 
 const router = Router();
 
-router.get('/contact-list', DisplayContactList);
+router.get('/contact-list', DisplayContactsList);
+router.get('/contact-add', DisplayContactsAddPage);
 
 export default router;
